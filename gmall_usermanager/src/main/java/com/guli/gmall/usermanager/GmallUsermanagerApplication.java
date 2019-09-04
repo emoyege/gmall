52472a7fffd6d4b8f4421adb20213ec2657f17fa
rluaqo <rluaqo@foxmail.com>
2019-09-04 18:39:52 +0800

    001

---
+++ gmall_usermanager/src/main/java/com/guli/gmall/usermanager/GmallUsermanagerApplication.java
@@ -0,0 +1,13 @@
+package com.guli.gmall.usermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallUsermanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallUsermanagerApplication.class, args);
+    }
+
+}
